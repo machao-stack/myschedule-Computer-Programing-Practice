ce6d1f23587cbdb22a00df5b26b68b6fd0e21c9c
machao <maboruimachao@sjtu.edu.cn>
2026-07-24 17:16:50 +0800

    after adjusting CMakeLists.txt and built

---
+++ build/CMakeFiles/myschedule.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myschedule.dir/main.cpp.o"
+  "CMakeFiles/myschedule.dir/main.cpp.o.d"
+  "CMakeFiles/myschedule.dir/src/Task.cpp.o"
+  "CMakeFiles/myschedule.dir/src/Task.cpp.o.d"
+  "myschedule"
+  "myschedule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myschedule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
